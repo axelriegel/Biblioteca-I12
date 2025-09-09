170d908796ce24099ac54e12eec084934647c3df
Axel Riegel <axel@gmail.com>
2025-09-09 10:35:28 -0300

    9/9

---
+++ public/bbdd/bbdd.js
@@ -0,0 +1,33 @@
+const usuarios = [
+    {
+        "codigo" : codigoUsuario,
+        "dni" : dniUsuario,
+        "nombreYApellido" : nombreYApellidoUsuario,
+        "email" : emailUsuario,
+        "cargo" : cargoUsuario,
+        "contrasena" : contrasenaUsuario,
+    }
+];
+
+const insumo = [
+    {
+        "codigo" : codigoInsumo,
+        "familia" : familiaInsumo,
+        "nombre" : nombreInsumo,
+        "estado" : estadoInsumo,
+        "categoria" : categoriaInsumo,
+        "cantidad" : cantidadInsumo,
+    },
+    
+];
+
+const prestamo = [
+    {
+        "codigo" : codigoPrestamo,
+        "fecha" : fechaPrestamo,
+        "insumo" : codigoInsumo,
+        "cantidad" : cantidadPrestamo,
+        "destinatario" : destinatario,
+        "fechaLimite" : fechaLimite,
+    }
+];
